cc6dd3a74f009826dfa20cdf432fc4cefa2a3621
popson <popsonemmanuel99@gmail.com>
2023-07-24 17:36:24 +0400

    Initial commit

---
+++ client/src/store/index.js
@@ -0,0 +1,14 @@
+import { proxy } from 'valtio';
+
+const state = proxy({
+
+    intro: true,
+    color: '	#FF0000',
+    isLogoTexture: true,
+    isFullTexture: false,
+    logoDecal: './threejs.png',
+    fullDecal: './threejs.png',
+
+});
+
+export default state;
